f17da90b7ec8e95a0b430c7c74e1d7cf1001e03b
James Jowers <james@Lestrades-iMac.local>
2017-02-03 03:25:11 -0500

    Removed FlightKit

--- Prototype/Assets/FlightKit/FlightKit/Scripts/UI/LoadSceneButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class LoadSceneButton : MonoBehaviour {
-	public string sceneName;
-	public virtual void OnClick() {
-		SceneManager.LoadScene(sceneName);	
-	}
-}
